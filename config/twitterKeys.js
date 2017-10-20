52f388b805c69b789bfa4cd321afcc2fcec43c8f
Alex <alexandermasud@protonmail.com>
2017-10-20 10:30:25 +0200

    Fixade nycklar

--- config/twitterKeys.js
+++ config/twitterKeys.js
@@ -4,7 +4,4 @@ module.exports = {
     consumerSecret:'f9BBJlKmxPaBBv0wKDSkgypFLRPNVgbsINcWObmCaE0MZUcadE',
     callbackURL:'/auth/twitter/callback',
     
-    token: '918786539873849344-kNVYAHc85JCetPDeF0rmpbgGYn6v0rh',
-    tokenSecret: 'tOKiC10iw2054OHmHHoasRYgMccAbJVj82uozfn7CuG0X',
-    
 }
